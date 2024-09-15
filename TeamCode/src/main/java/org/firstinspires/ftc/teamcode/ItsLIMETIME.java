81a4705751f4e04bb4310f6f02f2c4045fcad670
roobootics <arick@solarflarerobotics.org>
2024-09-15 19:57:17 -0400

    Create ItsLIMETIME.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ItsLIMETIME.java
@@ -0,0 +1,14 @@
+
+import static java.lang.Math.max;
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import
+
+public class ItsLIMETIME extends LinearOpMode{
+    public void runOpMode(){
+
+    }
+}
