2384ffce3df21d1daaa085d539982ad4c0e7552d
roobootics <arick@solarflarerobotics.org>
2024-09-21 16:40:20 -0400

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinkageTets.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinkageTets.java
@@ -2,15 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-@TeleOp
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+@Autonomous
 public class LinkageTets extends LinearOpMode{
     @Override
-    public void runOpMode(){
+    public void runOpMode() throws InterruptedException {
         Servo linkage1 = hardwareMap.servo.get("linkage1");
         linkage1.setPosition(0.7);
-        if(gamepad1.a){
-            linkage1.setPosition(0);
-        }
+        Thread.sleep(2000);
+        linkage1.setPosition(0);
+
     }
 }
